3b3c77a25809aac12e51d4a7d224319a8a6b3950
Shivanshi Tyagi <shivanshityagi@icloud.com>
2025-05-07 16:29:36 +0530

    Update wagmi config

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,14 +1,20 @@
 import farcasterFrame from "@farcaster/frame-wagmi-connector";
 import { injected, coinbaseWallet, metaMask } from "wagmi/connectors";
-import { http, createConfig } from "wagmi";
-import { base, mainnet } from "wagmi/chains";
+import { http, fallback, createConfig } from "wagmi";
+import { mainnet } from "wagmi/chains";
 
 export const config = createConfig({
-  chains: [base, mainnet],
+  chains: [mainnet],
   connectors: [farcasterFrame(), injected(), coinbaseWallet(), metaMask()],
   transports: {
-    [base.id]: http(),
-    [mainnet.id]: http(),
+    [mainnet.id]: fallback([
+      http(import.meta.env.VITE_DRPC_1),
+      http("https://rpc.ankr.com/eth"),
+      http("https://eth.llamarpc.com"),
+      http("https://ethereum.publicnode.com"),
+      http("https://cloudflare-eth.com"),
+      http(),
+    ]),
   },
 });
 
