e90813871912d7b1a16a59890c52baf7b4f7bc1c
Shivanshi Tyagi <shivanshityagi@icloud.com>
2025-05-27 16:57:06 +0700

    Add git action

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    allowedHosts: true,
+  build: {
+    sourcemap: false, // set explicitly
   },
 });
