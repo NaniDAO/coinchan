1a50115cc94ddd1831f22152987cbe3d155d6f39
Shivanshi Tyagi <shivanshityagi@icloud.com>
2025-06-02 21:27:40 +0700

    Add token-search

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,36 +7,68 @@ import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { CoinNani } from "@/components/coinnani";
 import { useTranslation } from "react-i18next";
+import { TokenCommandMenu } from "@/components/TokenCommandMenu";
+import { useTheme } from "@/lib/theme";
+import { cn } from "@/lib/utils";
 
 export const Route = createRootRoute({
   component: () => {
     const { t } = useTranslation();
+    const { theme } = useTheme();
+
     return (
       <>
-        <header className="p-2 flex items-center justify-between w-full gap-5">
+        <header
+          className={cn(
+            "p-2 flex items-center justify-between w-full gap-5 border-b border-white/10 dark:border-white/10 shadow-md bg-gradient-to-tr",
+            theme === "dark"
+              ? "from-neutral-900 via-neutral-900 to-neutral-950"
+              : "from-yellow-50 via-yellow-100 to-yellow-100",
+          )}
+        >
           {/* Desktop Navigation */}
-          <nav className="hidden md:flex items-center justify-center space-x-4">
-            <Link to="/" className="[&.active]:font-bold uppercase hover:text-primary transition-colors">
-              📈 {t("common.swap")}
+          <nav
+            className={cn(
+              "backdrop-blur-lg px-2 rounded-lg hidden md:flex w-full items-center justify-center space-x-4",
+              theme === "dark" ? "bg-black/20" : "bg-white/10",
+            )}
+          >
+            <Link
+              to="/"
+              className="[&.active]:font-bold uppercase hover:text-primary transition-colors flex flex-row w-full space-x-1 items-center justify-center"
+            >
+              <span>📈</span> <span>{t("common.swap")}</span>
             </Link>
             <span className="">/</span>
-            <Link to="/explore" className="[&.active]:font-bold uppercase hover:text-primary transition-colors">
-              🗺️ {t("common.explore")}
+            <Link
+              to="/explore"
+              className="[&.active]:font-bold uppercase hover:text-primary transition-colors flex flex-row w-full space-x-1 items-center justify-center"
+            >
+              <span>🗺️</span> <span>{t("common.explore")}</span>
             </Link>
             <span className="">/</span>
-            <Link to="/orders" className="[&.active]:font-bold uppercase hover:text-primary transition-colors">
-              📋 {t("common.orders")}
+            <Link
+              to="/orders"
+              className="[&.active]:font-bold uppercase hover:text-primary transition-colors flex flex-row w-full space-x-1 items-center justify-center"
+            >
+              <span>📋</span> <span>{t("common.orders")}</span>
             </Link>
             <span className="">/</span>
-            <Link to="/send" className="[&.active]:font-bold uppercase hover:text-primary transition-colors">
-              🪁 {t("common.send")}
+            <Link
+              to="/send"
+              className="[&.active]:font-bold uppercase hover:text-primary transition-colors flex flex-row w-full space-x-1 items-center justify-center"
+            >
+              <span>🪁</span> <span>{t("common.send")}</span>
             </Link>
             <span className="">/</span>
-            <Link to="/create" className="[&.active]:font-bold uppercase hover:text-primary transition-colors">
-              ✨ {t("common.create")}
+            <Link
+              to="/create"
+              className="[&.active]:font-bold uppercase hover:text-primary transition-colors flex flex-row w-full space-x-1 items-center justify-center"
+            >
+              <span>✨</span> <span>{t("common.create")}</span>
             </Link>
           </nav>
-
+          <TokenCommandMenu />
           <div className="flex items-center gap-2">
             <ConnectMenu />
             <ThemeToggle />
@@ -51,19 +83,34 @@ export const Route = createRootRoute({
             </SheetTrigger>
             <SheetContent side="right" className="p-2">
               <nav className="flex flex-col space-y-4 mt-8">
-                <Link to="/" className="[&.active]:font-bold uppercase hover:text-primary transition-colors">
+                <Link
+                  to="/"
+                  className="[&.active]:font-bold uppercase hover:text-primary transition-colors"
+                >
                   📈 {t("common.swap")}
                 </Link>
-                <Link to="/explore" className="[&.active]:font-bold uppercase hover:text-primary transition-colors">
+                <Link
+                  to="/explore"
+                  className="[&.active]:font-bold uppercase hover:text-primary transition-colors"
+                >
                   🗺️ {t("common.explore")}
                 </Link>
-                <Link to="/orders" className="[&.active]:font-bold uppercase hover:text-primary transition-colors">
+                <Link
+                  to="/orders"
+                  className="[&.active]:font-bold uppercase hover:text-primary transition-colors"
+                >
                   📋 {t("common.orders")}
                 </Link>
-                <Link to="/send" className="[&.active]:font-bold uppercase hover:text-primary transition-colors">
+                <Link
+                  to="/send"
+                  className="[&.active]:font-bold uppercase hover:text-primary transition-colors"
+                >
                   🪁 {t("common.send")}
                 </Link>
-                <Link to="/create" className="[&.active]:font-bold uppercase hover:text-primary transition-colors">
+                <Link
+                  to="/create"
+                  className="[&.active]:font-bold uppercase hover:text-primary transition-colors"
+                >
                   ✨ {t("common.create")}
                 </Link>
               </nav>
