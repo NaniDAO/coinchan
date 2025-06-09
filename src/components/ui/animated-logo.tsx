1b6f69ac61074d0ea432e6fbbf517ef3cebd1356
ross <92001561+z0r0z@users.noreply.github.com>
2025-06-09 12:15:28 +0000

    better alignment

--- src/components/ui/animated-logo.tsx
+++ src/components/ui/animated-logo.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const animatedLogoVariants = cva(
-  "inline-block transition-opacity duration-100 ease-in-out",
+  "inline-block transition-opacity duration-100 ease-in-out relative",
   {
     variants: {
       size: {
@@ -29,7 +29,7 @@ const AnimatedLogo = React.forwardRef<HTMLDivElement, AnimatedLogoProps>(
   ({ className, size, animated = false, ...props }, ref) => {
     return (
       <div
-        className={cn(animatedLogoVariants({ size }), "relative cursor-pointer", className)}
+        className={cn(animatedLogoVariants({ size }), "cursor-pointer", className)}
         ref={ref}
         {...props}
       >
@@ -121,415 +121,95 @@ const AnimatedLogo = React.forwardRef<HTMLDivElement, AnimatedLogoProps>(
           />
         </svg>
 
-        {/* Animated Logo */}
+        {/* Animated Logo - same as static but with rotation animation */}
         <svg
-          viewBox="0 0 400 1000"
+          viewBox="0 0 200 240"
           xmlns="http://www.w3.org/2000/svg"
           className={cn(
             "w-full h-full absolute top-0 left-0 transition-opacity duration-100 ease-in-out pointer-events-none",
-            animated ? "opacity-100" : "opacity-0"
+            animated ? "opacity-100 animate-spin" : "opacity-0"
           )}
+          style={{
+            animationDuration: animated ? "3.2s" : undefined,
+          }}
         >
-          {/* Frame 1: 0° - Front view */}
-          <g opacity="1">
-            <animate
-              attributeName="opacity"
-              values="1;0;0;0;0;0;0;0;0;0;0;0;0;0;0;0"
-              dur="3.2s"
-              repeatCount="indefinite"
-            />
-            <polygon
-              points="200,80 155,160 200,135"
-              fill="#FF6B9D"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,80 200,135 245,160"
-              fill="#00D4FF"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="155,160 200,135 200,160"
-              fill="#FFE066"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,135 245,160 200,160"
-              fill="#66D9A6"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="155,160 200,160 200,240"
-              fill="#FF9F40"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,160 245,160 200,240"
-              fill="#B967DB"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="200"
-              y1="80"
-              x2="200"
-              y2="240"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="155"
-              y1="160"
-              x2="245"
-              y2="160"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-          </g>
-
-          {/* Frame 2: 22.5° */}
-          <g opacity="0">
-            <animate
-              attributeName="opacity"
-              values="0;1;0;0;0;0;0;0;0;0;0;0;0;0;0;0"
-              dur="3.2s"
-              repeatCount="indefinite"
-            />
-            <polygon
-              points="200,80 160,160 200,132"
-              fill="#FF6B9D"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,80 200,132 240,160"
-              fill="#00D4FF"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="160,160 200,132 200,160"
-              fill="#FFE066"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,132 240,160 200,160"
-              fill="#66D9A6"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="160,160 200,160 200,240"
-              fill="#FF9F40"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,160 240,160 200,240"
-              fill="#B967DB"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="200"
-              y1="80"
-              x2="200"
-              y2="240"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="160"
-              y1="160"
-              x2="240"
-              y2="160"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-          </g>
-
-          {/* Frame 3: 45° */}
-          <g opacity="0">
-            <animate
-              attributeName="opacity"
-              values="0;0;1;0;0;0;0;0;0;0;0;0;0;0;0;0"
-              dur="3.2s"
-              repeatCount="indefinite"
-            />
-            <polygon
-              points="200,80 170,160 200,130"
-              fill="#FF6B9D"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,80 200,130 235,160"
-              fill="#00D4FF"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,130 235,160 200,160"
-              fill="#66D9A6"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="170,160 200,160 200,240"
-              fill="#FF9F40"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,160 235,160 200,240"
-              fill="#B967DB"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="200"
-              y1="80"
-              x2="200"
-              y2="240"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="170"
-              y1="160"
-              x2="235"
-              y2="160"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-          </g>
-
-          {/* Frame 4: 67.5° */}
-          <g opacity="0">
-            <animate
-              attributeName="opacity"
-              values="0;0;0;1;0;0;0;0;0;0;0;0;0;0;0;0"
-              dur="3.2s"
-              repeatCount="indefinite"
-            />
-            <polygon
-              points="200,80 180,160 200,135"
-              fill="#FF6B9D"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,80 200,135 220,160"
-              fill="#00D4FF"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,135 220,160 200,160"
-              fill="#66D9A6"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="180,160 200,160 200,240"
-              fill="#FF9F40"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,160 220,160 200,240"
-              fill="#B967DB"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="200"
-              y1="80"
-              x2="200"
-              y2="240"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="180"
-              y1="160"
-              x2="220"
-              y2="160"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-          </g>
-
-          {/* Frame 5: 90° - side view */}
-          <g opacity="0">
-            <animate
-              attributeName="opacity"
-              values="0;0;0;0;1;0;0;0;0;0;0;0;0;0;0;0"
-              dur="3.2s"
-              repeatCount="indefinite"
-            />
-            <polygon
-              points="200,80 195,160 200,140"
-              fill="#00D4FF"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,80 200,140 205,160"
-              fill="#66D9A6"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="195,160 200,160 200,240"
-              fill="#B967DB"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,160 205,160 200,240"
-              fill="#B967DB"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="200"
-              y1="80"
-              x2="200"
-              y2="240"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="195"
-              y1="160"
-              x2="205"
-              y2="160"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-          </g>
-
-          {/* Continue with remaining frames 6-16... */}
-          {/* For brevity, I'll include just a few more key frames */}
-          
-          {/* Frame 9: 180° - back view */}
-          <g opacity="0">
-            <animate
-              attributeName="opacity"
-              values="0;0;0;0;0;0;0;0;1;0;0;0;0;0;0;0"
-              dur="3.2s"
-              repeatCount="indefinite"
-            />
-            <polygon
-              points="200,80 155,160 200,135"
-              fill="#66D9A6"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,80 200,135 245,160"
-              fill="#FFE066"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="155,160 200,135 200,160"
-              fill="#66D9A6"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,135 245,160 200,160"
-              fill="#FFE066"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="155,160 200,160 200,240"
-              fill="#B967DB"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,160 245,160 200,240"
-              fill="#FF9F40"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="200"
-              y1="80"
-              x2="200"
-              y2="240"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="155"
-              y1="160"
-              x2="245"
-              y2="160"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-          </g>
-
-          {/* Frame 13: 270° - other side view */}
-          <g opacity="0">
-            <animate
-              attributeName="opacity"
-              values="0;0;0;0;0;0;0;0;0;0;0;0;1;0;0;0"
-              dur="3.2s"
-              repeatCount="indefinite"
-            />
-            <polygon
-              points="200,80 195,160 200,140"
-              fill="#FFE066"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,80 200,140 205,160"
-              fill="#66D9A6"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="195,160 200,160 200,240"
-              fill="#FF9F40"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <polygon
-              points="200,160 205,160 200,240"
-              fill="#FF9F40"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="200"
-              y1="80"
-              x2="200"
-              y2="240"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-            <line
-              x1="195"
-              y1="160"
-              x2="205"
-              y2="160"
-              stroke="#000000"
-              strokeWidth="2"
-            />
-          </g>
+          {/* Top left facet */}
+          <polygon
+            points="100,20 55,120 100,85"
+            fill="#FF6B9D"
+            stroke="#000000"
+            strokeWidth="2"
+          />
+          {/* Top right facet */}
+          <polygon
+            points="100,20 100,85 145,120"
+            fill="#00D4FF"
+            stroke="#000000"
+            strokeWidth="2"
+          />
+          {/* Middle left facet */}
+          <polygon
+            points="55,120 100,85 100,120"
+            fill="#FFE066"
+            stroke="#000000"
+            strokeWidth="2"
+          />
+          {/* Middle right facet */}
+          <polygon
+            points="100,85 145,120 100,120"
+            fill="#66D9A6"
+            stroke="#000000"
+            strokeWidth="2"
+          />
+          {/* Bottom left facet */}
+          <polygon
+            points="55,120 100,120 100,200"
+            fill="#FF9F40"
+            stroke="#000000"
+            strokeWidth="2"
+          />
+          {/* Bottom right facet */}
+          <polygon
+            points="100,120 145,120 100,200"
+            fill="#B967DB"
+            stroke="#000000"
+            strokeWidth="2"
+          />
+          {/* Center vertical line */}
+          <line
+            x1="100"
+            y1="20"
+            x2="100"
+            y2="200"
+            stroke="#000000"
+            strokeWidth="2"
+          />
+          {/* Top internal lines */}
+          <line
+            x1="100"
+            y1="85"
+            x2="55"
+            y2="120"
+            stroke="#000000"
+            strokeWidth="2"
+          />
+          <line
+            x1="100"
+            y1="85"
+            x2="145"
+            y2="120"
+            stroke="#000000"
+            strokeWidth="2"
+          />
+          {/* Middle horizontal line */}
+          <line
+            x1="55"
+            y1="120"
+            x2="145"
+            y2="120"
+            stroke="#000000"
+            strokeWidth="2"
+          />
         </svg>
       </div>
     );
