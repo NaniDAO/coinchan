e4a94e81113170684856a79dec875695d29e339f
Shivanshi Tyagi <shivanshityagi@icloud.com>
2025-07-02 15:54:24 +0700

    Nit ConnectButton styling

--- src/components/RainbowConnectButton.tsx
+++ src/components/RainbowConnectButton.tsx
@@ -1,5 +1,83 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { Button } from "@/components/ui/button";
 
 export const RainbowConnectButton = () => {
-  return <ConnectButton label="Login" />;
+  // @TODO internationalization
+  return (
+    <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        const ready = mounted && authenticationStatus !== "loading";
+        const connected =
+          ready &&
+          account &&
+          chain &&
+          (!authenticationStatus || authenticationStatus === "authenticated");
+
+        return (
+          <div
+            {...(!ready && {
+              "aria-hidden": true,
+              style: {
+                opacity: 0,
+                pointerEvents: "none",
+                userSelect: "none",
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <Button
+                    onClick={openConnectModal}
+                    type="button"
+                    variant="outline"
+                    size="sm"
+                  >
+                    Login
+                  </Button>
+                );
+              }
+
+              if (chain.unsupported) {
+                return (
+                  <Button
+                    onClick={openChainModal}
+                    size="sm"
+                    variant="destructive"
+                    className="text-destructive"
+                  >
+                    Wrong Network
+                  </Button>
+                );
+              }
+
+              return (
+                <div className="flex gap-2 justify-center items-center">
+                  <Button
+                    onClick={openAccountModal}
+                    variant="outline"
+                    size="sm"
+                    type="button"
+                  >
+                    {account.displayName}
+                    {account.displayBalance
+                      ? ` (${account.displayBalance})`
+                      : ""}
+                  </Button>
+                </div>
+              );
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom>
+  );
 };
