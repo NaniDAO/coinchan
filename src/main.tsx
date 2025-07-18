70bf09e9bfac8ead8b64a821e9356344f2fdcedc
ross <92001561+z0r0z@users.noreply.github.com>
2025-07-18 13:43:29 +0000

    improve connection handler

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import { WagmiProvider } from "wagmi";
 import "./index.css";
 import "./i18n";
 import { useTranslation } from "react-i18next";
+import ConnectionErrorHandler from "./lib/ConnectionErrorHandler";
 
 // Configure query client with performance optimizations
 const queryClient = new QueryClient({
@@ -84,6 +85,7 @@ export const WalletProviders = ({
                 })
           }
         >
+          <ConnectionErrorHandler />
           {children}
         </RainbowKitProvider>
       </QueryClientProvider>
