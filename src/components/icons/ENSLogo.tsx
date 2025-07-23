9deefa8a721917c27e3d73ff9dad1b28d4b2ab27
ross <92001561+z0r0z@users.noreply.github.com>
2025-07-23 13:35:06 +0000

    nit

--- src/components/icons/ENSLogo.tsx
+++ src/components/icons/ENSLogo.tsx
@@ -1,5 +1,5 @@
 export const ENSLogo = ({ className = "h-4 w-4" }: { className?: string }) => (
-y  <svg className={className} viewBox="0 0 202 231" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg className={className} viewBox="0 0 202 231" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
       d="M98.3592 2.80337L34.8353 107.327C34.3371 108.147 33.1797 108.238 32.5617 107.505C26.9693 100.864 6.13478 72.615 31.9154 46.8673C55.4403 23.3726 85.4045 6.62129 96.5096 0.831705C97.7695 0.174847 99.0966 1.59007 98.3592 2.80337Z"
       fill="currentColor"
