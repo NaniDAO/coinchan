af53240768c6a0458c29e7e8aee43533535a28e2
Shivanshi Tyagi <shivanshityagi@icloud.com>
2025-09-24 17:05:27 +0700

    🥢 Orders Page

--- src/routes/explore/orders.tsx
+++ src/routes/explore/orders.tsx
@@ -1,18 +1,13 @@
 import { OrdersPage } from "@/components/OrdersPage";
 import { createFileRoute } from "@tanstack/react-router";
-import { useTranslation } from "react-i18next";
 
 export const Route = createFileRoute("/explore/orders")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const { t } = useTranslation();
-
   return (
     <div className="p-5">
-      <h2 className="text-center mb-5 font-display uppercase tracking-[2px]">═══ {t("orders.title")} ═══</h2>
-
       <div className="orders-container">
         <OrdersPage />
       </div>
